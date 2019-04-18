888f971f703098c153f225120e2491cd0e4a7c6f
Sumner Fields <sumnertfields@gmail.com>
2019-04-18 10:52:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function logShout(string) {return console.log(string.toUpperCase())}
 
 function logWhisper(string) {return console.log(string.toLowerCase())}
 
-function sayHiToGrandma(string) {if string === string.toUpperCase() return "I CAN'T HEAR YOU!"
+function sayHiToGrandma(string) {if string === "I love you"
+  
+  else string === string.toUpperCase() return "I CAN'T HEAR YOU!"
   
   else string === string.toLowerCase() return "HELLO, INDEED!"
   
