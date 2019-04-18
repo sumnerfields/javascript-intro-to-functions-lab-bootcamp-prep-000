6f56e577dc8fa89b0b7a65d04041667f31db5843
Sumner Fields <sumnertfields@gmail.com>
2019-04-18 10:42:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ function shout(string) {return string.toUpperCase()}
 
 function whisper(string) {return string.toLowerCase()}
 
-function logShout(string) 
-{
-  return console.log(string.toUpperCase())
-}
+function logShout(string) {return console.log(string.toUpperCase())}
 
-function logWhisper(string) 
-{ return console.log(string.toLowerCase())
-}
+function logWhisper(string) { return console.log(string.toLowerCase())}
 
